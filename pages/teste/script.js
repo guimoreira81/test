54953e32245a8f91a96ba13915f2879f92b0e93d
Guilherme Moreira <79773621+guimoreira81@users.noreply.github.com>
2026-04-11 19:12:55 +0000

    saving code modifications

--- pages/teste/script.js
+++ pages/teste/script.js
@@ -47,21 +47,24 @@ let screenSize = new Vector2(process.stdout.columns, process.stdout.rows);
 
 let screen = {
     size: new Vector2(process.stdout.columns, process.stdout.rows),
-    screen: [],
+    screenCharacteres: [],
     fill: (char="x") => {
-        screen = [];
+        screen.screenCharacteres = [];
         for (let i = 0; i<screenSize.x*screenSize.y; i++){
-            screen.push(char);
+            screen.screenCharacteres.push(char);
         }
     },
     drawPixel: (char, position) => {
-
+        position = new Vector2(Math.abs(position.x), Math.abs(position.y));
+        let index = position.y*screenSize.x+position.x;
+        screen.screenCharacteres[index] = char;
     },
     refresh(){
+        process.stdout.write('\x1Bc');
         for (let y = 0; y<screen.size.y; y++){
             let line = ""
             for (let x = 0; x<screen.size.x; x++){
-                line += screen.screen[y*screen.size.x+x];   
+                line += screen.screenCharacteres[y*screen.size.x+x];   
             }
             console.log(line);
         }
@@ -70,24 +73,49 @@ let screen = {
 
 process.stdout.on('resize', () => {
     const {columns, rows} = process.stdout;
-    screen.size = new Vector2(columns, rows);
+    screenSize = new Vector2(columns, rows);
 });
-
 
 
 const game = {
     FPS: 12,
-    updateFrame: () => {},
+    world: [],
+    updateFrame: (dt) => {},
     drawFrame: () => {},
     _loop: () => {
-        screen.fill("a");
-        screen.drawPixel("O", new Vector2(0, 0));
-        screen.refresh();
-        game.updateFrame();
+        game.updateFrame(1/game.FPS);
+        for (const object of game.world){
+            screen.drawPixel("O", object.position);
+        }
+        screen.fill(" ");
         game.drawFrame();
+        screen.refresh();
     },
     start: () => {
         setInterval(game._loop, 1/game.FPS*1000);
     },
 };
 game.start();
+
+class GameObject{
+    constructor(name, size, position){
+        this.name = name;
+        this.size = size;
+        this.position = position;
+        game.world.push(this);
+    }
+}
+
+let particle = new GameObject("aa", new Vector2(1, 1), new Vector2(0, 0));
+
+for (let i = 0; i<10; i++){
+    let particle = new GameObject(i, new Vector2(1, 1), new Vector2(Math.random()*10, Math.random()*10));
+}
+
+game.updateFrame = (dt) => {
+
+}
+
+game.drawFrame = () => {
+
+}
